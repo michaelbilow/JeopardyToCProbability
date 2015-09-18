dbb587bfddca75349c1ff311d856015d1872a0cb
michaelbilow <bilow@cs.ucla.edu>
2015-09-18 12:06:46 -0700

    Added README.md, renamed parameters more sensibly

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# JeopardyToCProbability
+# Jeopardy ToC Probability Computer
+
+Right now, there are 4 parameters:
+
+1. The number of wins that your favorite champ has right now (`CHAMP_WINS`)
+2. The number of regular Jeopardy! games left before the next ToC (`GAMES_LEFT`)
+3. The probability that your favorite champ will be passed (in dollars) 
+   by someone with the same number of wins (`PROBABILITY_CHAMP_WON_LESS_MONEY_WITH_SAME_VICTORIES`)
+4. The maximum number of new champions who could pass your favorite champ (`MAX_STRONGER_CHAMPS_TO_REMAIN_ELIGIBLE`)
+   for them to still remain eligible for the ToC.
+
+
+For example, if your favorite champ is in 10th place, and there's a teachers champion and a college champ,
+then potentially there are 13 spots available to regular Jeopardy! contestants. That means that there
+could be at most 2 more champions from regular Jeopardy! for your favorite champ to remain eligible.
--- ToC_prob.py
+++ ToC_prob.py
@@ -13,58 +13,65 @@ EMPIRICAL_PROBS = \
      0.010638298]
 
 SMOOTHED_PROBS = [0, .54, .26, .08, .05, .03, .015, .005, .005, .005, .01]
-CHAMP_WIN_PROBS = EMPIRICAL_PROBS
 
-assert sum(CHAMP_WIN_PROBS) == 1
 
-MY_WINS = 3
-BIGGER_THAN_MY_WIN_PROB = 0.1
-GAMES_LEFT = 100
-MAX_WINNERS_TO_REMAIN_ELIGIBLE = 1
-TOTAL_SITUATIONS = MAX_WINNERS_TO_REMAIN_ELIGIBLE + 2
+CHAMP_WINS = 3
+PROBABILITY_CHAMP_WON_LESS_MONEY_WITH_SAME_VICTORIES = 0.1
+GAMES_LEFT = 41
+MAX_STRONGER_CHAMPS_TO_REMAIN_ELIGIBLE = 1
+
+TOTAL_SITUATIONS = MAX_STRONGER_CHAMPS_TO_REMAIN_ELIGIBLE + 2
+
 
 import numpy as np
 
-output_array = np.zeros((GAMES_LEFT, TOTAL_SITUATIONS))
-output_array[0][0] = 1
-# print output_array
+for CHAMP_WIN_PROBS in (EMPIRICAL_PROBS, SMOOTHED_PROBS):
+    assert sum(CHAMP_WIN_PROBS) == 1
+    if CHAMP_WIN_PROBS == EMPIRICAL_PROBS:
+        print 'Empirical Estimates\n'
+    else:
+        print 'Smoothed (pessimistic) estimates\n'
+    output_array = np.zeros((GAMES_LEFT, TOTAL_SITUATIONS))
+    output_array[0][0] = 1
+    # print output_array
 
-for i in range(GAMES_LEFT - 1):
-    for j in range(len(CHAMP_WIN_PROBS)):
-        win_prob = CHAMP_WIN_PROBS[j]
-        for k in range(TOTAL_SITUATIONS):
-            if i + j >= GAMES_LEFT:
-                if j > MY_WINS and i + MY_WINS < GAMES_LEFT:
-                    output_array[-1][min(k + 1, TOTAL_SITUATIONS - 1)] += \
+    for i in range(GAMES_LEFT - 1):
+        for j in range(len(CHAMP_WIN_PROBS)):
+            win_prob = CHAMP_WIN_PROBS[j]
+            for k in range(TOTAL_SITUATIONS):
+                if i + j >= GAMES_LEFT:
+                    if j > CHAMP_WINS and i + CHAMP_WINS < GAMES_LEFT:
+                        output_array[-1][min(k + 1, TOTAL_SITUATIONS - 1)] += \
+                            output_array[i][k] * win_prob
+                    else:
+                        output_array[-1][k] += \
+                            output_array[i][k] * win_prob
+                elif j <= CHAMP_WINS - 1:
+                    output_array[i + j][k] += \
                         output_array[i][k] * win_prob
+                elif j == CHAMP_WINS:
+                    output_array[i + j][k] += \
+                        output_array[i][k] * win_prob * (1.0 - PROBABILITY_CHAMP_WON_LESS_MONEY_WITH_SAME_VICTORIES)
+                    output_array[i + j][min(k + 1, TOTAL_SITUATIONS - 1)] += \
+                        output_array[i][k] * win_prob * PROBABILITY_CHAMP_WON_LESS_MONEY_WITH_SAME_VICTORIES
                 else:
-                    output_array[-1][k] += \
+                    output_array[i + j][min(k + 1, TOTAL_SITUATIONS - 1)] += \
                         output_array[i][k] * win_prob
-            elif j <= MY_WINS - 1:
-                output_array[i + j][k] += \
-                    output_array[i][k] * win_prob
-            elif j == MY_WINS:
-                output_array[i + j][k] += \
-                    output_array[i][k] * win_prob * (1.0 - BIGGER_THAN_MY_WIN_PROB)
-                output_array[i + j][min(k + 1, TOTAL_SITUATIONS - 1)] += \
-                    output_array[i][k] * win_prob * BIGGER_THAN_MY_WIN_PROB
-            else:
-                output_array[i + j][min(k + 1, TOTAL_SITUATIONS - 1)] += \
-                    output_array[i][k] * win_prob
-                # print i
-                # print output_array
+                    # print i
+                    # print output_array
 
-# print output_array
-output_probs = output_array[-1]
-assert abs(output_probs.sum() - 1) < .001
+    # print output_array
+    output_probs = output_array[-1]
+    assert abs(output_probs.sum() - 1) < .001
 
-output_range = range(TOTAL_SITUATIONS)
+    output_range = range(TOTAL_SITUATIONS)
 
-format_str = 'Probability of {}{} additional winners in the next {} games: {:.2%}'
+    format_str = 'Probability of {}{} additional winners in the next {} games: {:.2%}'
 
-output_range_names = [format_str.format(i, '', GAMES_LEFT, output_probs[i]) for i in output_range[:-1]]
-output_range_names += [format_str.format(output_range[-1], ' or more', GAMES_LEFT, output_array[-1][-1])]
+    output_range_names = [format_str.format(i, '', GAMES_LEFT, output_probs[i]) for i in output_range[:-1]]
+    output_range_names += [format_str.format(output_range[-1], ' or more', GAMES_LEFT, output_array[-1][-1])]
 
-print '\n'.join(output_range_names)
-print '\n=============\n'
-print 'ToC Probability: {:.2%}'.format(output_probs[:-1].sum())
+    print '\n'.join(output_range_names)
+    print '\n=============\n'
+    print 'ToC Probability: {:.2%}'.format(output_probs[:-1].sum())
+    print '\n'
